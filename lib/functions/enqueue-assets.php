03987cfc555fa0c2598c0654daa1f7c02a90fc4e
Rose Cox <rose@purpleprodigy.com>
2018-04-10 17:37:24 +1000

    Update styles and configure widgets

--- lib/functions/enqueue-assets.php
+++ lib/functions/enqueue-assets.php
@@ -36,6 +36,7 @@ function noosablockcon_enqueue_styles() {
 // Enqueue LESS files to the UIkit compiler.
 	beans_compiler_add_fragment( 'uikit', array(
 		CHILD_LIB . '/assets/less/core/base.less',
+		CHILD_LIB . '/assets/less/core/block.less',
 		CHILD_LIB . '/assets/less/core/overlay.less',
 		CHILD_LIB . '/assets/less/core/variables.less',
 		CHILD_LIB . '/assets/less/layout/default.less',
@@ -98,5 +99,5 @@ add_action( 'wp_enqueue_scripts', 'noosablockcon_add_google_fonts' );
  * @return void
  */
 function noosablockcon_add_google_fonts() {
-	wp_enqueue_style( 'pp-google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:400,400i|Raleway:300,400,600', false );
+	wp_enqueue_style( 'pp-google-fonts', 'https://fonts.googleapis.com/css?family=Montserrat:400,700|Raleway:300,400,600', false );
 }
--- views/pre-footer.php
+++ views/pre-footer.php
@@ -1,5 +1,8 @@
-<div class="ehc-pre-footer-area uk-cover-background uk-flex uk-flex-center uk-flex-middle uk-overlay uk-width-1-1 uk-text-center">
-    <div class="uk-overlay-panel uk-overlay-background uk-overlay-fade uk-overlay-bottom">
-        <h2>Content</h2>
+<div class="nbc-pre-footer-area uk-cover-background uk-flex uk-flex-center uk-flex-middle uk-overlay uk-width-1-1 uk-text-center">
+    <div class="uk-overlay-panel uk-overlay-background uk-overlay-fade uk-overlay-bottom nbc-headline">
+        <h1>Noosa Block Con</h1>
+        <h2>June 2018 - July 2019</h2>
+        <h3>Creating a Friendly Space For Change-Makers, Visionaries, and Innovation</h3>
+        <a href="/about/" class="uk-button nbc-button">Find Out More</a>
     </div>
 </div>
