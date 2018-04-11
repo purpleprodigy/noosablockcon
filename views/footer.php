28c482371d2385df9998f903c4ed2bb57d2543cb
Rose Cox <rose@purpleprodigy.com>
2018-04-11 13:11:28 +1000

    Update url to https

--- views/footer.php
+++ views/footer.php
@@ -1,4 +1,4 @@
 <div class="uk-container-center uk-text-center nbc-footer">
-    <p>Copyright &copy; <?php echo date( 'Y' ); ?> <a href="http://noosablockcon.io" target="_blank">Noosa Block Con</a></p>
+    <p>Copyright &copy; <?php echo date( 'Y' ); ?> <a href="https://noosablockcon.io" target="_blank">Noosa Block Con</a></p>
     <p>Website by: <a href="https://purpleprodigy.com" target="_blank">Purple Prodigy</a></p>
 </div>
