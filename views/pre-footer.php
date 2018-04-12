3de9c21b6fb26e2c7570d9374da11a2ec1f0486e
Rose Cox <rose@purpleprodigy.com>
2018-04-12 10:07:02 +1000

    Update date

--- views/pre-footer.php
+++ views/pre-footer.php
@@ -1,7 +1,7 @@
 <div class="nbc-pre-footer-area uk-cover-background uk-flex uk-flex-center uk-flex-middle uk-overlay uk-width-1-1 uk-text-center">
     <div class="uk-overlay-panel uk-overlay-background uk-overlay-fade uk-overlay-bottom nbc-headline">
         <h1>Noosa Block Con</h1>
-        <h2>June 2018 - July 2019</h2>
+        <h2>June 2018 - June 2019</h2>
         <h3>Creating a Friendly Space For Change-Makers, Visionaries, and Innovation</h3>
         <a href="/about/" class="uk-button nbc-button">Find Out More</a>
     </div>
