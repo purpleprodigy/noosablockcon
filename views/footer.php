e3826b39df6d9fbf7750e3c6bc1023e89d079d56
Rose Cox <rose@purpleprodigy.com>
2018-08-06 11:01:01 +1000

    Update footer

--- views/footer.php
+++ views/footer.php
@@ -1,4 +1,3 @@
 <div class="uk-container-center uk-text-center nbc-footer">
     <p>Copyright &copy; <?php echo date( 'Y' ); ?> <a href="https://noosablockcon.io" target="_blank">Noosa Block Con</a></p>
-    <p>Website by: <a href="https://purpleprodigy.com" target="_blank">Purple Prodigy</a></p>
 </div>
